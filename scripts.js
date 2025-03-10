2a9f5f501e929a2d317a8c990771756d4e3f98d5
CHEASENGHOUR <cheasenghour789@gmail.com>
2025-03-10 15:10:59 +0700

    addAOS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-let sc = window.scrollY;
-const nav = document.getElementById('navbar');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    nav.style.top = "-100px";
-  } else {
-    nav.style.top = "0";
-  }
-  sc = window.scrollY;
-})
+AOS.init();
