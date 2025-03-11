ba2160aff219a5bfdbbfbfba02ce16c14c7f7ba8
CHEASENGHOUR <cheasenghour789@gmail.com>
2025-03-11 15:35:27 +0700

    addSwiper

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 AOS.init();
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  freemode: {
+    enabled: true,
+    sticky: true,
+  },
+  autoplay: {
+    delay: 1000,
+  },
+  spaceBetween: 20,
+  slidesPerView: 8,
+  navigation: {
+    nextEl: '#btn-next',
+    prevEl: '#btn-prev',
+  },
+});
