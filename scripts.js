7482e811705327f1dd8361d3bed389bc046032c9
CHEASENGHOUR <cheasenghour789@gmail.com>
2025-03-13 14:31:44 +0700

    breakpoints

--- scripts.js
+++ scripts.js
@@ -11,7 +11,25 @@ const swiper = new Swiper('.swiper', {
     delay: 1000,
   },
   spaceBetween: 20,
-  slidesPerView: 8,
+  slidesPerView: 2,
+  breakpoints: {
+    1400: {
+      spaceBetween: 20,
+      slidesPerView: 8,
+    },
+    1200: {
+      spaceBetween: 20,
+      slidesPerView: 6
+    },
+    992: {
+      spaceBetween: 20,
+      slidesPerView: 5,
+    },
+    768: {
+      spaceBetween: 20,
+      slidesPerView: 3,
+    },
+  },
   navigation: {
     nextEl: '#btn-next',
     prevEl: '#btn-prev',
